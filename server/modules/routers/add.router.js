17e4614d11d986847f5d309f187efcb82678b1fa
Ben Hacker <bhacker1995@gmail.com>
2018-06-14 12:54:23 -0500

    Added POST for add

--- server/modules/routers/add.router.js
+++ server/modules/routers/add.router.js
@@ -15,4 +15,18 @@ router.get( '/', ( req, res ) => {
     });
 })
 
+router.post( '/', ( req, res ) => {
+    console.log( 'In POST request for entry' );
+    let newEntry = req.body;
+    const queryText = `INSERT INTO entry ( entry, hours ) VALUES ( $1, $2 )`;
+    pool.query( queryText, [ newEntry.entry, newEntry.hours ] )
+    .then( ( result ) => {
+        console.log( `Successfully posted to database with ${ result }` );
+        res.sendStatus( 201 );
+    }).catch( ( error ) => {
+        console.log( `Error posting to power: ${ error }` );
+        res.sendStatus( 500 );
+    })
+})
+
 module.exports = router;
--- server/public/scripts/services/time-tracker.service.js
+++ server/public/scripts/services/time-tracker.service.js
@@ -4,6 +4,20 @@ timeApp.service( 'TimeService', [ '$http', function( $http ) {
 
     self.entryArray = { list: [] };
 
+
+    self.addEntry = function( object ){
+        return $http({
+            method: 'POST',
+            url: '/add',
+            data: self.newEntry
+        }).then(function( response ){
+            console.log( 'Posted POST for /add' );
+            self.getEntry();
+        }).catch(function( error ){
+            console.log( `Error handling POST for /add: ${ error }` );
+        });
+    } // end addEntry
+
     self.getEntry = function(){
         return $http({
             method: 'GET',
